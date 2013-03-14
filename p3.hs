c8369366a1ce313ab8b0ba0296cb0497d45f1a34
3noch <elliot@3noch.com>
2013-03-14 17:58:35 -0400

    Store sum instead of average
    Thanks @cstobey for this!

--- p3.hs
+++ p3.hs
@@ -8,18 +8,18 @@ import qualified Data.Text.IO as Tio (readFile)
 delim = '\t'
 
 -- An Entry describes the average and count of all input lines identified by `entryName`
-data Entry = Entry { entryName :: Text
-                   , entryAverage :: Double
+data Entry = Entry { entryName  :: Text
+                   , entrySum   :: Double
                    , entryCount :: Int}
 
 main :: IO ()
 main = do
     (fileName:_) <- getArgs
-    xs <- map parseLine . T.lines <$> Tio.readFile fileName
+    xs <- map lineToPair . T.lines <$> Tio.readFile fileName
     putStrLn $ columnizeFields "ID" "AVERAGE" "COUNT" "SUM"
     mapM_ (putStrLn . entryToString) (entries xs)
     where
-        entries xs = map hashToEntry (combine xs)
+        entries xs = map pairToEntry (combine xs)
 
 -- Takes four strings and places them in columns of varying widths for display purposes.
 columnizeFields :: String -> String -> String -> String -> String
@@ -28,21 +28,21 @@ columnizeFields f1 f2 f3 f4 = column 50 f1 ++ column 25 f2 ++ column 15 f3 ++ f4
 
 -- Renders a single entry to a string for display purposes.
 entryToString :: Entry -> String
-entryToString (Entry name average count) =
-    columnizeFields (T.unpack name)
-                    (show average)
-                    ('x' : show count)
-                    ('=' : show (fromIntegral count * average))
+entryToString (Entry name_ sum_ count_) =
+    columnizeFields (T.unpack name_)
+                    (show $ average sum_ count_)
+                    ('x' : show count_)
+                    ('=' : show sum_)
+    where average s c = realToFrac sum_ / fromIntegral count_
 
 -- Takes a key-value pair and transforms it into an entry. The key is the entry's name and the
 -- value is a list of times that all correspond to that entry.
-hashToEntry :: (Real v) => (Text, [v]) -> Entry
-hashToEntry (key, vs) = Entry key average count
-    where (average, count) = avgAndCount vs
+pairToEntry :: (Text, [Double]) -> Entry
+pairToEntry (key, vs) = Entry key sum_ count_ where (sum_, count_) = sumAndCount vs
 
 -- Parses a line from the input file and returns the two columns as a tuple.
-parseLine :: Text -> (Text, Double)
-parseLine str = (T.strip key, double)
+lineToPair :: Text -> (Text, Double)
+lineToPair str = (T.strip key, double)
     where (key, value) = T.span (/= delim) str
           double = read (T.unpack (T.strip value))
 
@@ -54,8 +54,7 @@ combine ((k,v):xs) = (k, v:values) : (combine nonmatching)
         (matching, nonmatching) = partition ((k==) . fst) xs
         values = map snd matching
 
--- Calculates the average and size of a list of numbers. This code could be much simpler, but this
+-- Calculates the sum and size of a list of numbers. This code could be much simpler, but this
 -- method was chosen after the simpler way was too slow and memory-intensive on large input files.
-avgAndCount :: (Real a, Fractional b, Integral c) => [a] -> (b, c)
-avgAndCount xs = (realToFrac total / fromIntegral count, count)
-    where (total, count) = foldl' (\(s, c) n -> (s+n, c+1)) (0, 0) xs
+sumAndCount :: (Real a, Integral c) => [a] -> (a, c)
+sumAndCount xs = foldl' step (0, 0) xs where step (s, c) n = (s+n, c+1)
